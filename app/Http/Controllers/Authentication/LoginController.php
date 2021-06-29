79d9458ffd12e6adc0c3e276737f72882568c278
Gabriel Okunola <aroicx@Gabriels-MacBook-Pro.local>
2021-06-29 20:00:23 +0100

    ...

--- app/Http/Controllers/Authentication/LoginController.php
+++ app/Http/Controllers/Authentication/LoginController.php
@@ -9,8 +9,8 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
-use Auth;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -26,60 +26,51 @@ class LoginController extends Controller
         $credentials = $request->only(['email', 'password']);
 
         $last_login = new Carbon();
-       
-        $last_ip =
-        array(
-           'user_ip' => $_SERVER['REMOTE_ADDR'],
-           'browserAgent' => $_SERVER['HTTP_USER_AGENT']
-       );
 
-
-
-      
-
+        $last_ip = [
+            'user_ip' => $_SERVER['REMOTE_ADDR'],
+            'browserAgent' => $_SERVER['HTTP_USER_AGENT'],
+        ];
 
         try {
-
-            $validate = User::where('email',$request->email)->first();
-            if(!$validate){
+            $validate = User::where('email', $request->email)->first();
+            if (!$validate) {
                 return [
                     'status' => 'info',
-                    'message' => 'Email or Password is incorrect'
+                    'message' => 'Email or Password is incorrect',
                 ];
-            }else if(!$validate->email_verified){
+            } elseif (!$validate->email_verified) {
                 return [
                     'status' => 'info',
-                    'message' => 'Please your verify email account.'
+                    'message' => 'Please your verify email account.',
                 ];
             }
 
             $token = Auth::guard()->attempt($credentials);
+
             // return $token;
 
-            if(!$token) {
+            if (!$token) {
                 throw new AccessDeniedHttpException();
             }
-
         } catch (JWTException $e) {
             throw new HttpException(500);
         }
 
-        // return User::where('user_id',Auth::user()->user_id)->first();
-        User::where('user_id',Auth::user()->user_id)->update([
+        User::where('user_id', Auth::user()->user_id)->update([
             'last_login' => $last_login->today()->toDateString(),
-            'last_ip_used' => json_encode($last_ip)]
-        );
+            'last_ip_used' => json_encode($last_ip),
+        ]);
 
-        $current_user = User::where('user_id',Auth::user()->user_id)->first();
+        $current_user = User::where('user_id', Auth::user()->user_id)->first();
 
-        return response()
-            ->json([
-                'status' => '200',
-                'user' => $current_user,
-                'message' => 'Authentication Successfully',
-                'token' => $token,
+        return response()->json([
+            'status' => '200',
+            'user' => $current_user,
+            'message' => 'Authentication Successfully',
+            'token' => $token,
 
-                // 'expires_in' => Auth::guard()->factory()->getTTL() * 60
-            ]);
+            // 'expires_in' => Auth::guard()->factory()->getTTL() * 60
+        ]);
     }
 }
